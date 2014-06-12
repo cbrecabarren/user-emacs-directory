eea04f51ab603727be7e7e50285d0b67dd52eeed
Carlos Beltrán-Recabarren <cbrecabarren@gmail.com>
2014-06-12 11:28:24 -0400

    Improves web-mode default config

--- mods/web.el
+++ mods/web.el
@@ -1,10 +1,18 @@
 (require 'web-mode)
 
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode)) 
-(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode)) 
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode)) 
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(defun web-mode-default-config ()
+  (setq web-mode-script-padding 2)
+)
+
+(setq web-mode-config-hook 'web-mode-default-config)
+
+(add-hook 'web-mode-hook (lambda () (run-hooks 'web-mode-config-hook)))
